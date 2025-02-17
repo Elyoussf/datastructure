e8cfb9fee730a104b2e810f445e986e0187cb514
Elyoussf <strangeamazigh1968@gmail.com>
2025-02-17 15:21:43 +0100

    ___-___

--- Arrays/Array.py
+++ Arrays/Array.py
@@ -1,63 +1,57 @@
-
-
 class Array:
-
-    def __init__(self,capacity):
+    def __init__(self, capacity):
         self.capacity = capacity
         self.size = 0
-        self.core = [None]*self.capacity
-    
-    
-    def add(self,e):
-        if self.capacity<=self.size:
-            l = [None]*self.capacity*2
-            for i in range(len(self.core)):
-                l[i] = self.core[i]
-            self.core = l
-            self.capacity = self.capacity*2
-        self.core.append(e)
-        self.size+=1
-    def delete_ByIndex(self,index):
+        self.core = [None] * self.capacity
 
+    def add(self, e):
+        if self.size >= self.capacity:
+            self._resize(self.capacity * 2)
+        self.core[self.size] = e
+        self.size += 1
+
+    def delete_ByIndex(self, index):
         if index >= self.size or index < -self.size:
-            print('Cannot delete , The index is out of range!')
-        else:
-            self.core.remove(index)
-            self.size = self.size - 1
+            print("Cannot delete, The index is out of range!")
+            return
+        if index < 0:
+            index += self.size  # Convert negative index to positive
 
-            if self.size < self.capacity/4:
-                self.core = self.core[:max(4*self.size+1,self.capacity)]
-                self.capacity = max(4*self.size+1,self.capacity)
+        for i in range(index, self.size - 1):
+            self.core[i] = self.core[i + 1]
 
-    
-    def get(self,index):
+        self.size -= 1
+        self.core[self.size] = None  # Remove reference to last element
+
+        if self.size < self.capacity // 4 and self.capacity > 4:
+            self._resize(max(4, self.capacity // 2))
+
+    def get(self, index):
         if index >= self.size or index < -self.size:
-            print('Cannot get the element , The index is out of range!')
-        else:
-            return self.core[index]
-        
-    def set(self,index,val):
-        if self.capacity<=self.size:
-            l = [None]*self.capacity*2
-            for i in range(len(self.core)):
-                l[i] = self.core[i]
-            self.core = l
-            self.capacity = self.capacity*2
-        if index > self.size or index < -self.size:
-            print('The index is not valid ')
-        else:
-            self.core.insert(index,val)
-            self.size+=1
+            print("Cannot get the element, The index is out of range!")
+            return None
+        return self.core[index if index >= 0 else self.size + index]
+
+    def set(self, index, val):
+        if index >= self.size or index < -self.size:
+            print("The index is not valid")
+            return
+        self.core[index if index >= 0 else self.size + index] = val
+
+    def _resize(self, new_capacity):
+        new_core = [None] * new_capacity
+        for i in range(self.size):
+            new_core[i] = self.core[i]
+        self.core = new_core
+        self.capacity = new_capacity
 
 
+# Test the fixed implementation
 arr = Array(4)
 
-arr.set(0,1)
-arr.set(0,1)
-
-print(arr.get(1))
-        
-
-
-        
+arr.add(1)
+arr.add(2)
+arr.add(3)
+arr.add(4)
+arr.add(5)  # Triggers resizing
 
